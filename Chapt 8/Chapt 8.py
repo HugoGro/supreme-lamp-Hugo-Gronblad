81d9f051e75f3ee1d5c25d92aae8ffd17f5e4222
Administratör <hugo.gronblad@ubf.haninge.se>
2026-03-05 15:29:35 +0100

    Hej

--- Chapt 8/Chapt 8.py
+++ Chapt 8/Chapt 8.py
@@ -1,12 +1,14 @@
-def area(a,b):
-    return a,b
-a = 6
-b = 5
+#uppgift 8.1
 
-ar = (a * a * 3.14)
-cir = (b * 2 * 3.14)
-print(ar)
-print(cir)
+#def area(a,b):
+    #return a,b
+#a = 6
+#b = 5
+
+#r = (a * a * 3.14)
+#cir = (b * 2 * 3.14)
+#print(ar)
+#print(cir)
 
 
 
